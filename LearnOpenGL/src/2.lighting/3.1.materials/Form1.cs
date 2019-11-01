52c6d5bd0637f260ae655f27a50daf23f9f21786
sailings <sailings@126.com>
2019-11-01 17:35:13 +0800

    添加材质

---
+++ LearnOpenGL/src/2.lighting/3.1.materials/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SharpGL;
+using SharpGL.SceneGraph.Assets;
+using SharpGL.Shaders;
+using SharpGL.VertexBuffers;
+using GlmNet;
+
+namespace _3._1.materials
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 屏幕宽度
+        /// </summary>
+        private const int SCR_WIDTH = 800;
+
+        /// <summary>
+        /// 屏幕高度
+        /// </summary>
+        private const int SCR_HEIGHT = 600;
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        private string title = "LearnOpenGL";
+
+        /// <summary>
+        /// OpenGL实例
+        /// </summary>
+        private OpenGL GL;
+
+        /// <summary>
+        /// 顶点
+        /// </summary>
+        private float[] vertices = {
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+    };
+
+        /// <summary>
+        /// 立方体Shader
+        /// </summary>
+        private ShaderProgram cubeShaderProgram = new ShaderProgram();
+
+        /// <summary>
+        /// 光源Shader
+        /// </summary>
+        private ShaderProgram lampShaderProgram = new ShaderProgram();
+
+        /// <summary>
+        /// 立方体VAO
+        /// </summary>
+        VertexBufferArray cubeVAO = new VertexBufferArray();
+
+        /// <summary>
+        /// 光源VAO
+        /// </summary>
+        VertexBufferArray lightVAO = new VertexBufferArray();
+
+        private DateTime startTime = DateTime.Now;
+
+        //摄像机对象
+        Camera camera = new Camera(new vec3(0.0f, 0.0f, 3.0f), new vec3(0.0f, 1.0f, 0.0f));
+
+        float lastX = SCR_WIDTH / 2.0f;
+        float lastY = SCR_HEIGHT / 2.0f;
+        bool firstMouse = true;
+
+        float deltaTime = 0.0f;
+        float lastFrame = 0.0f;
+
+        /// <summary>
+        /// 光源位置
+        /// </summary>
+        vec3 lightPos = new vec3(1.2f, 1.0f, 2.0f);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            camera.Zoom=90.0f;
+
+            openGLControl1.MouseWheel += OpenGLControl1_MouseWheel;
+            openGLControl1.MouseMove += OpenGLControl1_MouseMove;
+        }
+
+        private void OpenGLControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //以下获取offset
+                var xpos = e.X;
+                var ypos = e.Y;
+
+                if (firstMouse)
+                {
+                    lastX = xpos;
+                    lastY = ypos;
+                    firstMouse = false;
+                }
+
+                float xoffset = xpos - lastX;
+                float yoffset = lastY - ypos;
+
+                lastX = xpos;
+                lastY = ypos;
+
+                //传递给camera处理
+                camera.ProcessMouseMovement(xoffset, yoffset);
+            }
+        }
+
+        private void OpenGLControl1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            var yoffset = e.Delta / Math.Abs(e.Delta);
+            camera.ProcessMouseScroll(yoffset);
+        }
+
+        private float GetTime()
+        {
+            return (float)(DateTime.Now - startTime).TotalSeconds;
+        }
+
+        /// <summary>
+        /// OpenGL绘制事件内容
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OpenGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            float currentFrame = GetTime();
+            deltaTime = currentFrame - lastFrame;
+            lastFrame = currentFrame;
+
+            //清除，以颜色填充
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            //清除颜色缓冲，深度缓冲
+            GL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //使用立方体着色器
+            GL.UseProgram(cubeShaderProgram.ShaderProgramObject);
+
+            //lightPos.x = 1.0f + glm.sin(GetTime()) * 2.0f;
+            //lightPos.y = glm.sin(GetTime() / 2.0f) * 1.0f;
+
+            ////设置着色器中的颜色值
+            //cubeShaderProgram.SetUniform3(GL,"objectColor", 1.0f, 0.5f, 0.31f);
+            //cubeShaderProgram.SetUniform3(GL, "lightColor", 1.0f, 1.0f, 1.0f);
+            //cubeShaderProgram.SetUniform3(GL, "lightPos", lightPos.x, lightPos.y, lightPos.z);
+            //cubeShaderProgram.SetUniform3(GL,"viewPos", camera.Position.x, camera.Position.y, camera.Position.z);
+
+            cubeShaderProgram.SetUniform3(GL, "light.position", lightPos.x, lightPos.y, lightPos.z);
+            cubeShaderProgram.SetUniform3(GL, "viewPos", camera.Position.x, camera.Position.y, camera.Position.z);
+
+            // light properties
+            vec3 lightColor = new vec3();
+            lightColor.x = glm.sin(GetTime() * 2.0f);
+            lightColor.y = glm.sin(GetTime() * 0.7f);
+            lightColor.z = glm.sin(GetTime() * 1.3f);
+            vec3 diffuseColor = lightColor * new vec3(0.5f);
+            vec3 ambientColor = diffuseColor * new vec3(0.2f);
+            cubeShaderProgram.SetUniform3(GL, "light.ambient", ambientColor.x, ambientColor.y, ambientColor.z);
+            cubeShaderProgram.SetUniform3(GL, "light.diffuse", diffuseColor.x, diffuseColor.y, diffuseColor.z);
+            cubeShaderProgram.SetUniform3(GL, "light.specular", 1.0f, 1.0f, 1.0f);
+
+            cubeShaderProgram.SetUniform3(GL, "material.ambient", 1.0f, 0.5f, 0.31f);
+            //cubeShaderProgram.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+            cubeShaderProgram.SetUniform3(GL, "material.diffuse", 1.0f, 0.5f, 0.31f);
+            //cubeShaderProgram.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+            cubeShaderProgram.SetUniform3(GL, "material.specular", 0.5f, 0.5f, 0.5f);
+            //cubeShaderProgram.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+            cubeShaderProgram.SetUniform1(GL, "material.shininess", 32.0f);
+            //cubeShaderProgram.setFloat("material.shininess", 32.0f);
+
+            //传递三个矩阵到着色器中
+            mat4 projection = glm.perspective(glm.radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            mat4 view = camera.GetViewMatrix();
+            cubeShaderProgram.SetUniformMatrix4(GL,"projection", projection.to_array());
+            cubeShaderProgram.SetUniformMatrix4(GL,"view", view.to_array());
+            mat4 model = new mat4(2.0f);
+            cubeShaderProgram.SetUniformMatrix4(GL,"model",model.to_array());
+
+            //绑定立方体VAO
+            cubeVAO.Bind(GL);
+
+            //绘制
+            GL.DrawArrays(OpenGL.GL_TRIANGLES, 0, 36);
+
+            //使用光源着色器
+            GL.UseProgram(lampShaderProgram.ShaderProgramObject);
+
+            //传递三个矩阵到着色器中
+            lampShaderProgram.SetUniformMatrix4(GL,"projection", projection.to_array());
+            lampShaderProgram.SetUniformMatrix4(GL,"view", view.to_array());
+            model = new mat4(1.0f);
+            model = glm.translate(model, lightPos);
+            model = glm.scale(model, new vec3(0.2f));
+            lampShaderProgram.SetUniformMatrix4(GL,"model", model.to_array());
+
+            //绑定光源VAO
+            lightVAO.Bind(GL);
+
+            //绘制
+            GL.DrawArrays(OpenGL.GL_TRIANGLES, 0, 36);
+
+            //设置标题，显示FPS
+            Text = title + $"-FPS[{openGLControl1.FPS}]";
+        }
+
+        /// <summary>
+        /// OpenGL初始化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+            //获取OpenGL对象
+            GL = openGLControl1.OpenGL;
+
+            //启用深度测试
+            GL.Enable(OpenGL.GL_DEPTH_TEST);
+
+            //创建两个Shader
+            cubeShaderProgram.Create(GL, "3.1.materials.vs", "3.1.materials.fs");
+            lampShaderProgram.Create(GL, "3.1.lamp.vs", "3.1.lamp.fs");
+
+            //创建立方体VAO
+            cubeVAO.Create(GL);
+
+            //绑定立方体VAO
+            cubeVAO.Bind(GL);
+
+            //创建vbo
+            var vbo = new VertexBuffer();
+            vbo.Create(GL);
+
+            //绑定vbo
+            vbo.Bind(GL);
+
+            //绑定数据
+            GL.BufferData(OpenGL.GL_ARRAY_BUFFER, vertices, OpenGL.GL_STATIC_DRAW);
+
+            //配置顶点属性
+            GL.VertexAttribPointer(0, 3, OpenGL.GL_FLOAT, false, 6 * sizeof(float), IntPtr.Zero);
+            GL.EnableVertexAttribArray(0);
+
+            GL.VertexAttribPointer(1, 3, OpenGL.GL_FLOAT, false, 6 * sizeof(float), new IntPtr(3*sizeof(float)));
+            GL.EnableVertexAttribArray(1);
+
+            //创建光源VAO
+            lightVAO.Create(GL);
+
+            //绑定光源VAO
+            lightVAO.Bind(GL);
+
+            //将vbo绑定到当前的光源VAO
+            vbo.Bind(GL);
+
+            //绑定数据
+            GL.VertexAttribPointer(0, 3, OpenGL.GL_FLOAT, false, 6 * sizeof(float), IntPtr.Zero);
+            GL.EnableVertexAttribArray(0);
+
+            //设置窗体的大小
+            Size = new Size(SCR_WIDTH, SCR_HEIGHT);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            //根据按下的键，更新摄像机位置信息
+            if (keyData == Keys.W)
+                camera.ProcessKeyboard(Camera_Movement.FORWARD, deltaTime);
+            if (keyData == Keys.S)
+                camera.ProcessKeyboard(Camera_Movement.BACKWARD, deltaTime);
+            if (keyData == Keys.A)
+                camera.ProcessKeyboard(Camera_Movement.LEFT, deltaTime);
+            if (keyData == Keys.D)
+                camera.ProcessKeyboard(Camera_Movement.RIGHT, deltaTime);
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
