984a9f0be8672bcd9a5ad12392ef9d4324b81d0f
sailings <sailings@126.com>
2019-11-05 09:59:35 +0800

    模型加载代码添加注释信息

--- LearnOpenGL/src/3.model_loading/1.model_loading/Mesh.cs
+++ LearnOpenGL/src/3.model_loading/1.model_loading/Mesh.cs
@@ -10,19 +10,40 @@ using SharpGL.VertexBuffers;
 
 namespace _1.model_loading
 {
+    /// <summary>
+    /// 顶点信息
+    /// </summary>
     public struct Vertex
     {
-        // position
+        /// <summary>
+        /// 位置
+        /// </summary>
         public vec3 Position { get; set; }
-        // normal
+
+        /// <summary>
+        /// 法线
+        /// </summary>
         public vec3 Normal { get; set; }
-        // texCoords
+        
+        /// <summary>
+        /// 纹理坐标
+        /// </summary>
         public vec2 TexCoords { get; set; }
-        // tangent
+
+        /// <summary>
+        /// 切线
+        /// </summary>
         public vec3 Tangent { get; set; }
-        // bitangent
+
+        /// <summary>
+        /// 二重切线
+        /// </summary>
         public vec3 Bitangent { get; set; }
 
+        /// <summary>
+        /// 转换成数组
+        /// </summary>
+        /// <returns></returns>
         public float[] ToArray()
         {
             var pArray = Position.to_array().ToList();
@@ -38,127 +59,121 @@ namespace _1.model_loading
             result.AddRange(bArray);
             return result.ToArray();
         }
-    };
+    }
+
+    /// <summary>
+    /// 纹理
+    /// </summary>
     public struct Texture
     {
+        /// <summary>
+        /// ID
+        /// </summary>
         public int id { get; set; }
+
+        /// <summary>
+        /// 纹理类型
+        /// </summary>
         public string type { get; set; }
+
+        /// <summary>
+        /// 纹理路径
+        /// </summary>
         public string path { get; set; }
-    };
+    }
+
+    /// <summary>
+    /// 网格
+    /// </summary>
     public class Mesh
     {
-    /*  Mesh Data  */
+        /// <summary>
+        /// 顶点
+        /// </summary>
         public Vertex[] vertices { get; set; }
+
+        /// <summary>
+        /// 顶点索引
+        /// </summary>
         public int[] indices { get; set; }
+
+        /// <summary>
+        /// 纹理
+        /// </summary>
         public Texture[] textures { get; set; }
 
+        /// <summary>
+        /// OpenGL实例
+        /// </summary>
         private OpenGL gl;
-
-        //uint VAO;
 
         /// <summary>
         /// vao
         /// </summary>
         VertexBufferArray vao = new VertexBufferArray();
 
-        /*  Functions  */
-        // constructor
         public Mesh(Vertex[] vertices, int[] indices, Texture[] textures,OpenGL gl)
         {
             this.vertices = vertices;
             this.indices = indices;
             this.textures = textures;
             this.gl = gl;
-            // now that we have all the required data, set the vertex buffers and its attribute pointers.
             SetupMesh();
         }
 
-        // render the mesh
+        /// <summary>
+        /// 绘制
+        /// </summary>
+        /// <param name="shader">所用shader</param>
+        /// <param name="gl">OpenGL实例</param>
         public void Draw(ShaderProgram shader, OpenGL gl)
         {
-            // bind appropriate textures
+            //以下绑定贴图
             int diffuseNr = 1;
             int specularNr = 1;
             int normalNr = 1;
             int heightNr = 1;
             for (int i = 0; i < textures.Length; i++)
             {
-                gl.ActiveTexture((uint)(OpenGL.GL_TEXTURE0 + i)); // active proper texture unit before binding
-                                                                  // retrieve texture number (the N in diffuse_textureN)
+                //激活纹理单元
+                gl.ActiveTexture((uint)(OpenGL.GL_TEXTURE0 + i)); 
                 string number = "";
                 string name = textures[i].type;
-                if (name == "texture_diffuse")
+                if (name == "texture_diffuse")  //漫反射贴图
                     number = (diffuseNr++).ToString();
-                else if (name == "texture_specular")
-                    number = (specularNr++).ToString(); // transfer unsigned int to stream
-                else if (name == "texture_normal")
-                    number = (normalNr++).ToString(); // transfer unsigned int to stream
-                else if (name == "texture_height")
-                    number = (heightNr++).ToString(); // transfer unsigned int to stream
+                else if (name == "texture_specular")    //高光贴图
+                    number = (specularNr++).ToString();
+                else if (name == "texture_normal")  //法线贴图
+                    number = (normalNr++).ToString();
+                else if (name == "texture_height")  //高度图
+                    number = (heightNr++).ToString();
 
-                // now set the sampler to the correct texture unit
-                //glUniform1i(glGetUniformLocation(shader.ShaderObject, (name + number)), i);
-                //gl.Uniform1(gl.GetUniformLocation(shader.ShaderObject, name + number), i);
+                //将纹理单元传递给shader
                 shader.SetUniform1(gl, name + number, i);
-                // and finally bind the texture
+
+                //绑定纹理
                 gl.BindTexture(OpenGL.GL_TEXTURE_2D, (uint)textures[i].id);
             }
 
-            // draw mesh
-            //gl.BindVertexArray(VAO);
+            //绑定vao
             vao.Bind(gl);
-            gl.DrawElements(OpenGL.GL_TRIANGLES, indices.Length, indices.Select(x => (uint)x).ToArray());
-            gl.BindVertexArray(0);
 
-            // always good practice to set everything back to defaults once configured.
-            gl.ActiveTexture(OpenGL.GL_TEXTURE0);
+            //绘制
+            gl.DrawElements(OpenGL.GL_TRIANGLES, indices.Length, indices.Select(x => (uint)x).ToArray());
+
+            //解绑
+            gl.BindVertexArray(0);
         }
 
-        /*  Render data  */
-        //VertexBuffer vbo = new VertexBuffer();
-        //VertexBuffer ebo = new VertexBuffer();
-
-        /*  Functions    */
-        // initializes all the buffer objects/arrays
+        /// <summary>
+        /// 适配网格
+        /// </summary>
         void SetupMesh()
         {
-            //// create buffers/arrays
-            //gl.GenVertexArrays(1, &VAO);
-            //gl.GenBuffers(1, &VBO);
-            //glGenBuffers(1, &EBO);
-
-            //glBindVertexArray(VAO);
-            //// load data into vertex buffers
-            //glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            //// A great thing about structs is that their memory layout is sequential for all its items.
-            //// The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
-            //// again translates to 3/2 floats which translates to a byte array.
-            //glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
-
-            //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-            //glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
-
-            //// set the vertex attribute pointers
-            //;
-            //// vertex Positions
-            //glEnableVertexAttribArray(0);
-            //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-            //// vertex normals
-            //glEnableVertexAttribArray(1);
-            //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-            //// vertex texture coords
-            //glEnableVertexAttribArray(2);
-            //glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
-            //// vertex tangent
-            //glEnableVertexAttribArray(3);
-            //glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Tangent));
-            //// vertex bitangent
-            //glEnableVertexAttribArray(4);
-            //glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Bitangent));
-
-            //glBindVertexArray(0);
-
+            //创建vao
             vao.Create(gl);
+
+            //绑定vao
             vao.Bind(gl);
 
             //创建vbo
@@ -168,29 +183,37 @@ namespace _1.model_loading
             //绑定vbo
             vbo.Bind(gl);
 
+            //获取定点数据
             float[] data = vertices.SelectMany(v => v.ToArray()).ToArray();
+
             //绑定数据
             gl.BufferData(OpenGL.GL_ARRAY_BUFFER, data, OpenGL.GL_STATIC_DRAW);
 
-            //配置顶点属性
+            //以下配置顶点属性
+            //位置
             gl.VertexAttribPointer(0, 3, OpenGL.GL_FLOAT, false, Marshal.SizeOf(typeof(Vertex)), IntPtr.Zero);
             gl.EnableVertexAttribArray(0);
 
             unsafe
-            {                
+            {     
+                //法线
                 gl.VertexAttribPointer(1, 3, OpenGL.GL_FLOAT, false, Marshal.SizeOf(typeof(Vertex)), new IntPtr(sizeof(vec3)));
                 gl.EnableVertexAttribArray(1);
 
+                //纹理坐标
                 gl.VertexAttribPointer(2, 2, OpenGL.GL_FLOAT, false, Marshal.SizeOf(typeof(Vertex)), new IntPtr(sizeof(vec3) * 2));
                 gl.EnableVertexAttribArray(2);
 
+                //切线
                 gl.VertexAttribPointer(3, 3, OpenGL.GL_FLOAT, false, Marshal.SizeOf(typeof(Vertex)), new IntPtr(sizeof(vec3) * 2 + sizeof(vec2)));
                 gl.EnableVertexAttribArray(3);
 
+                //二重切线
                 gl.VertexAttribPointer(4, 3, OpenGL.GL_FLOAT, false, Marshal.SizeOf(typeof(Vertex)), new IntPtr(sizeof(vec3) * 3 + sizeof(vec2)));
                 gl.EnableVertexAttribArray(4);
             }
 
+            //解绑
             vao.Unbind(gl);
         }
     }
--- LearnOpenGL/src/3.model_loading/1.model_loading/Model.cs
+++ LearnOpenGL/src/3.model_loading/1.model_loading/Model.cs
@@ -10,208 +10,228 @@ using System.Text;
 
 namespace _1.model_loading
 {
-    class Model
+    /// <summary>
+    /// 模型
+    /// </summary>
+    public class Model
     {
-        /*  Model Data */
-        List<Texture> textures_loaded = new List<Texture>();    // stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
-        List<Mesh> meshes = new List<Mesh>();
-        string directory;
-        string fileName;
-        bool gammaCorrection;
+        /// <summary>
+        /// 已加载纹理
+        /// </summary>
+        private List<Texture> textures_loaded = new List<Texture>();
+
+        /// <summary>
+        /// 网格集合
+        /// </summary>
+        private List<Mesh> meshes = new List<Mesh>();
+
+        /// <summary>
+        /// 模型所在路径
+        /// </summary>
+        private string directory;
+
+        /// <summary>
+        /// 模型名称
+        /// </summary>
+        private string fileName;
+
+        /// <summary>
+        /// OpenGL实例
+        /// </summary>
         private OpenGL gl;
+
+        /// <summary>
+        /// Assimp上下文
+        /// </summary>
         AssimpContext context = new AssimpContext();
 
-        /*  Functions   */
-        // constructor, expects a filepath to a 3D model.
-        public Model(string path, OpenGL gl, bool gamma = false)
+        public Model(string path, OpenGL gl)
         {
             fileName = path;
             this.gl = gl;
-            gammaCorrection = gamma;
             FileInfo fInfo = new FileInfo(fileName);
             directory = fInfo.Directory.Name;
             LoadModel(path);
         }
 
-        // draws the model, and thus all its meshes
+        /// <summary>
+        /// 绘制
+        /// </summary>
+        /// <param name="shader">使用的shander</param>
         public void Draw(ShaderProgram shader)
         {
+            //以此绘制网格
             for (int i = 0; i < meshes.Count; i++)
                 meshes[i].Draw(shader, gl);
         }
 
-        /*  Functions   */
-        // loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
-        void LoadModel(string path)
+        /// <summary>
+        /// 加载模型
+        /// </summary>
+        /// <param name="path"></param>
+        private void LoadModel(string path)
         {
-            // read file via ASSIMP
-            //Assimp::Importer importer;
-            //const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
-            //// check for errors
-            //if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
-            //{
-            //    cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << endl;
-            //    return;
-            //}
-            // retrieve the directory path of the filepath
+            //使用AssimpContext加载模型
             var scene = context.ImportFile(path, PostProcessSteps.Triangulate | PostProcessSteps.FlipUVs | PostProcessSteps.CalculateTangentSpace);
 
-            //directory = path.substr(0, path.find_last_of('/'));
-
-            // process ASSIMP's root node recursively
+            //递归处理场景中的节点
             ProcessNode(scene.RootNode, scene);
         }
 
-        // processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+        /// <summary>
+        /// 递归处理场景中的节点
+        /// </summary>
+        /// <param name="node">根节点</param>
+        /// <param name="scene">场景</param>
         void ProcessNode(Node node, Scene scene)
         {
-            // process each mesh located at the current node
+            //处理当前节点
             for (int i = 0; i < node.MeshCount; i++)
             {
-                // the node object only contains indices to index the actual objects in the scene. 
-                // the scene contains all the data, node is just to keep stuff organized (like relations between nodes).
-                //var mesh = scene->mMeshes[node->mMeshes[i]];
-                //var mesh = scene.Meshes[i];
+                //获取对应的网格
                 var mesh = scene.Meshes.Find(a => a.Name == node.Name);
+
+                //加入网格列表
                 meshes.Add(ProcessMesh(mesh, scene));
-                //meshes.push_back(ProcessMesh(mesh, scene));
             }
-            // after we've processed all of the meshes (if any) we then recursively process each of the children nodes
+
+            //处理子节点
             for (int i = 0; i < node.ChildCount; i++)
             {
                 ProcessNode(node.Children[i], scene);
             }
         }
 
-        Mesh ProcessMesh(Assimp.Mesh mesh, Scene scene)
+        /// <summary>
+        /// 处理Mesh
+        /// </summary>
+        /// <param name="mesh">网格</param>
+        /// <param name="scene">场景</param>
+        /// <returns></returns>
+        private Mesh ProcessMesh(Assimp.Mesh mesh, Scene scene)
         {
-            // data to fill
+            //顶点
             List<Vertex> vertices = new List<Vertex>();
+
+            //索引
             List<int> indices = new List<int>();
+
+            //纹理
             List<Texture> textures = new List<Texture>();
 
-            // Walk through each of the mesh's vertices
             for (int i = 0; i < mesh.VertexCount; i++)
             {
+                //顶点
                 Vertex vertex = new Vertex();
-                vec3 vector = new vec3(); // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
-                                          // positions
 
+                //位置信息
+                vec3 vector = new vec3();
                 vector.x = mesh.Vertices[i].X;
                 vector.y = mesh.Vertices[i].Y;
                 vector.z = mesh.Vertices[i].Z;
-
                 vertex.Position = vector;
-                // normals
+
+                //法线信息
                 vector.x = mesh.Normals[i].X;
                 vector.y = mesh.Normals[i].Y;
                 vector.z = mesh.Normals[i].Z;
                 vertex.Normal = vector;
-                // texture coordinates
+
+                //纹理信息
                 if (mesh.HasTextureCoords(0))
-                //if (mesh->mTextureCoords[0]) // does the mesh contain texture coordinates?
                 {
                     vec2 vec = new vec2();
-                    // a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't 
-                    // use models where a vertex can have multiple texture coordinates so we always take the first set (0).
-
                     vec.x = mesh.TextureCoordinateChannels[0][i].X;
                     vec.y = mesh.TextureCoordinateChannels[0][i].Y;
                     vertex.TexCoords = vec;
                 }
                 else
                     vertex.TexCoords = new vec2(0.0f, 0.0f);
-                // tangent
 
+                //切线信息
                 vector.x = mesh.Tangents[i].X;
                 vector.y = mesh.Tangents[i].Y;
                 vector.z = mesh.Tangents[i].Z;
                 vertex.Tangent = vector;
-                // bitangent
 
+                //二重切线信息
                 vector.x = mesh.BiTangents[i].X;
                 vector.y = mesh.BiTangents[i].Y;
                 vector.z = mesh.BiTangents[i].Z;
                 vertex.Bitangent = vector;
                 vertices.Add(vertex);
             }
-            // now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
+
+            //顶点索引信息
             for (int i = 0; i < mesh.FaceCount; i++)
             {
                 Face face = mesh.Faces[i];
-                // retrieve all indices of the face and store them in the indices vector
                 for (int j = 0; j < face.IndexCount; j++)
                     indices.Add(face.Indices[j]);
             }
-            // process materials
+            
+            //处理材质相关
             Material material = scene.Materials[mesh.MaterialIndex];
-            // we assume a convention for sampler names in the shaders. Each diffuse texture should be named
-            // as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER. 
-            // Same applies to other texture as the following list summarizes:
-            // diffuse: texture_diffuseN
-            // specular: texture_specularN
-            // normal: texture_normalN
 
-            // 1. diffuse maps
-
+            //漫反射贴图
             List<Texture> diffuseMaps = LoadMaterialTextures(material, TextureType.Diffuse, "texture_diffuse");
-            //textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
             textures.AddRange(diffuseMaps);
-            // 2. specular maps
 
+            //高光贴图
             List<Texture> specularMaps = LoadMaterialTextures(material, TextureType.Specular, "texture_specular");
-            //textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
             textures.AddRange(specularMaps);
-            // 3. normal maps
 
+            //法线贴图
             List<Texture> normalMaps = LoadMaterialTextures(material, TextureType.Normals, "texture_normal");
-            //textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
             textures.AddRange(normalMaps);
-            // 4. height maps
 
+            //高度图
             List<Texture> heightMaps = LoadMaterialTextures(material, TextureType.Height, "texture_height");
-            //textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
             textures.AddRange(heightMaps);
 
-            // return a mesh object created from the extracted mesh data
+            //生成新的网格
             return new Mesh(vertices.ToArray(), indices.ToArray(), textures.ToArray(), gl);
         }
 
-        // checks all material textures of a given type and loads the textures if they're not loaded yet.
-        // the required info is returned as a Texture struct.
+        /// <summary>
+        /// 价值材质纹理
+        /// </summary>
+        /// <param name="mat">材质</param>
+        /// <param name="type">纹理类型</param>
+        /// <param name="typeName">类型名称</param>
+        /// <returns>纹理集合</returns>
         List<Texture> LoadMaterialTextures(Material mat, TextureType type, string typeName)
         {
             List<Texture> textures = new List<Texture>();
             for (int i = 0; i < mat.GetMaterialTextureCount(type); i++)
             {
-                //aiString str;
-                //mat->GetTexture(type, i, &str);
+                //获取纹理
                 mat.GetMaterialTexture(type, i, out TextureSlot textureTmp);
-                // check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
                 bool skip = false;
                 for (int j = 0; j < textures_loaded.Count(); j++)
                 {
+                    //避免重复加载
                     if (textures_loaded[j].path == textureTmp.FilePath)
-                    //if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
                     {
                         textures.Add(textures_loaded[j]);
-                        //textures.push_back(textures_loaded[j]);
-                        skip = true; // a texture with the same filepath has already been loaded, continue to next one. (optimization)
+                        skip = true;
                         break;
                     }
                 }
                 if (!skip)
-                {   // if texture hasn't been loaded already, load it
+                {   
+                    //加载纹理
                     SharpGL.SceneGraph.Assets.Texture txure = new SharpGL.SceneGraph.Assets.Texture();
                     txure.Create(gl, directory + "\\" + textureTmp.FilePath);
-                    //txure.TextureName
+
+                    //生成新的纹理对象
                     Texture texture = new Texture();
-                    //texture.id = TextureFromFile(str.C_Str(), this->directory);
                     texture.id = (int)txure.TextureName;
                     texture.type = typeName;
-                    //texture.path = str.C_Str();
                     texture.path = textureTmp.FilePath;
                     textures.Add(texture);
+
+                    //加入纹理集合
                     textures_loaded.Add(texture);
                 }
             }
